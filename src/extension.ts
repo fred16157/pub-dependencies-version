2b8ff7c997570f315a6a9c12b4f9d57ff51cb47a
강현구 <fred16157@gmail.com>
2021-05-31 09:17:30 +0900

    빌드 결과 정리 및 자동완성 기능 추가

--- src/extension.ts
+++ src/extension.ts
@@ -5,28 +5,29 @@ import PackageInfo from './package-info';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('activated!');
-	context.subscriptions.push(vscode.languages.registerCompletionItemProvider({ language: 'yaml', scheme: 'file', pattern: '**/pubspec.yml' }, {
-		async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-			console.log('starting autocomplete');
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider({ language: 'yaml', scheme: 'file' }, {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 			let items: vscode.CompletionItem[] = [];
 			let line = document.lineAt(position.line);
-			let dependencyName = line.text.split(':')[0];
+			let dependencyName = line.text.split(':')[0].trim();
 			const pubServer = 'https://pub.dartlang.org';
-			let res = await axios.get(`${pubServer}/api/packages/${dependencyName}`, { headers: [{ accept: "application/vnd.pub.v2+json" }] });
-			const packageInfo = new PackageInfo(JSON.parse(res.data));
-			for (let version of packageInfo.versions) {
-				if (version === packageInfo.latest) {
-					let item = new vscode.CompletionItem(`${version} (최신)`, vscode.CompletionItemKind.Constant);
-					item.insertText = new vscode.SnippetString(version);
-					items.push(item);
+			return new Promise(async (resolve, reject) => {
+				let res = await axios.get(`${pubServer}/api/packages/${dependencyName}`);
+				const packageInfo = new PackageInfo(res.data);
+				for (let version of packageInfo.versions) {
+					if (version === packageInfo.latest) {
+						let item = new vscode.CompletionItem(`${version} (latest)`, vscode.CompletionItemKind.Constant);
+						item.insertText = new vscode.SnippetString(version);
+						items.push(item);
+					}
+					else {
+						let item = new vscode.CompletionItem(`${version}`, vscode.CompletionItemKind.Constant);
+						item.insertText = new vscode.SnippetString(version);
+						items.push(item);
+					}
 				}
-				else {
-					let item = new vscode.CompletionItem(`${version}`, vscode.CompletionItemKind.Constant);
-					item.insertText = new vscode.SnippetString(version);
-					items.push(item);
-				}
-			}
-			return items;
+				resolve(items);
+			});
 		}
 	}, ''));
 }
